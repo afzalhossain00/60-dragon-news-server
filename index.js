c6fca72b2519b16013f24a7aeaf306a614a97893
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-12-04 12:55:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
+app.get('/', (req, res) => {
+    res.send('Hello Express')
+})
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const catetoryNews = news.filter(n => n.category_id === id)
+        res.send(catetoryNews)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+    console.log(req.params.id);
+})
+
+app.listen(port, () => {
+    console.log('Dragon News Server running on port', port);
+})
